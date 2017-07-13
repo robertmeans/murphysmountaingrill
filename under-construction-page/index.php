7407a957b2fb27ef35d0178cb7f85edb24dac2b5
robertmeans <robert@robertmeans.com>
2017-07-13 08:44:33 -0600

    updated menus

--- under-construction-page/index.php
+++ under-construction-page/index.php
@@ -55,9 +55,9 @@
 	<h2>Click for larger image</h2><br />
 <div class="menu-wrap cf">
 
-	<a class="reset" href="_images/murphys-apps.gif" target="_blank"><img src="_images/murphys-apps-thumbnail.gif" alt="Appetizers"></a>
-	<a class="reset" href="_images/murphys-drinks.gif" target="_blank"><img src="_images/murphys-drinks-thumbnail.gif" alt="Drinks"></a>
-	<a class="reset" href="_images/murphys-entrees.gif" target="_blank"><img src="_images/murphys-entrees-thumbnail.gif" alt="Entreés"></a>
+	<a class="reset" href="_images/murphys-apps-02.gif" target="_blank"><img src="_images/murphys-apps-thumbnail-02.gif" alt="Appetizers"></a>
+	<a class="reset" href="_images/murphys-drinks-02.gif" target="_blank"><img src="_images/murphys-drinks-thumbnail-02.gif" alt="Drinks"></a>
+	<a class="reset" href="_images/murphys-entrees-02.gif" target="_blank"><img src="_images/murphys-entrees-thumbnail-02.gif" alt="Entreés"></a>
 
 </div><!-- .instruction-wrap -->
 
